d8a5e20d201f6b48bc4fba74690849d354d925f3
Sam Ezebunandu <sam.ezebunandu@gmail.com>
2025-07-15 22:50:01 -0600

    a package-oriented approach to printing hello world

---
+++ hello/hello_test.go
@@ -0,0 +1,19 @@
+package hello_test
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/ezebunandu/hello"
+)
+
+func TestPrint__PrintsHelloMessageToGivenWriter(t *testing.T){
+    t.Parallel()
+    buf := new(bytes.Buffer)
+    hello.PrintTo(buf)
+    want := "hello world\n"
+    got := buf.String()
+    if want != got {
+        t.Fatalf("want %q, got %q", want, got) 
+    }
+}
