fcd13d0494cd8cd6fb4932878028429d677ccfa8
Sam Ezebunandu <sam.ezebunandu@gmail.com>
2025-07-17 22:15:52 -0600

    count: a package for counting lines in input

---
+++ count/count_test.go
@@ -0,0 +1,20 @@
+package count_test
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/ezebunandu/count"
+)
+
+func TestLines__CountsLinesInInput(t *testing.T){
+    t.Parallel()
+
+    c := count.NewCounter()
+    c.Input = bytes.NewBufferString("1\n2\n3")
+    want := 3
+    got := c.Lines()
+    if want != got {
+        t.Errorf("want %d, got %d", want, got)
+    }
+}
