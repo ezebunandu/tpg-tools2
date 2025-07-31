28b7a35931ce99928fbcf40ec359f9de31fe347d
Sam Ezebunandu <sam.ezebunandu@gmail.com>
2025-07-31 06:20:33 -0600

    writer: test for error and file clobbering

--- writer/writer_test.go
+++ writer/writer_test.go
@@ -24,3 +24,33 @@ func TestWriteToFile__WritesGivenDataToFile(t *testing.T){
         t.Fatal(cmp.Diff(want, got))
     }
 }
+
+func TestWriteToFile__ReturnsErrorForUnwritableFile(t *testing.T){
+    t.Parallel()
+    path := "bogus/write_test.txt"
+    err := writer.WriteToFile(path, []byte{})
+    if err == nil {
+        t.Fatal("want error when file not writable, got none")
+    }
+}
+
+func TestWriteToFile__ClobbersExistingFile(t *testing.T){
+    t.Parallel()
+    path := t.TempDir() + "/write_test.txt"
+    err := os.WriteFile(path, []byte{4, 5, 6}, 0o600)
+    if err != nil {
+        t.Fatal(err)
+    }
+    want := []byte{1, 2, 3}
+    err = writer.WriteToFile(path, want)
+    if err != nil {
+        t.Fatal(err)
+    }
+    got, err := os.ReadFile(path)
+    if err != nil {
+        t.Fatal(err)
+    }
+    if !cmp.Equal(want, got){
+        t.Fatal(cmp.Diff(want, got))
+    }
+}
