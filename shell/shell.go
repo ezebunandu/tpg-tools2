2c0e0bccba7924ec616018acc58d3afb35b1c29a
Sam Ezebunandu <sam.ezebunandu@gmail.com>
2025-08-13 08:50:55 -0600

    shell: Session represents a shell session

--- shell/shell.go
+++ shell/shell.go
@@ -2,6 +2,8 @@ package shell
 
 import (
 	"fmt"
+	"io"
+	"os"
 	"os/exec"
 	"strings"
 )
@@ -11,10 +13,29 @@ import (
 type cmd struct {
     Args []string
 }
+
+type Session struct {
+    Stdin io.Reader
+    Stdout io.Writer
+    Stderr io.Writer
+}
+
+func NewSession(input io.Reader, output, err io.Writer) *Session {
+    return &Session{Stdin: input, Stdout: output, Stderr: err}
+}
+
 func CmdFromString(input string) (*exec.Cmd, error) {
     args := strings.Fields(input)
     if len(args) < 1 {
         return  nil, fmt.Errorf("empty input %s", input )
     }
     return exec.Command(args[0], args[1:]...), nil
+}
+
+func (session *Session) Run() {}
+
+func Main() int {
+    session := NewSession(os.Stdin, os.Stdout, os.Stderr)
+    session.Run()
+    return  0
 }
--- shell/shell_test.go
+++ shell/shell_test.go
@@ -1,10 +1,11 @@
 package shell_test
 
 import (
+	"os"
 	"testing"
 
+	"github.com/ezebunandu/shell"
 	"github.com/google/go-cmp/cmp"
-    "github.com/ezebunandu/shell"
 )
 
 func TestCmdFromString__CreatesExpectedCmd(t *testing.T){
@@ -27,5 +28,18 @@ func TestCmdFromString__ErrorsOnEmptyInput(t *testing.T){
     _, err := shell.CmdFromString(input)
     if err == nil {
         t.Fatal("want error on empty input, got nil")
+    }
+}
+
+func TestNewSession__CreatesExpectedSession(t *testing.T){
+    t.Parallel()
+    want := shell.Session{
+        Stdin: os.Stdin,
+        Stdout: os.Stdout,
+        Stderr: os.Stderr,
+    }
+    got := *shell.NewSession(os.Stdin, os.Stdout, os.Stderr)
+    if want != got {
+        t.Errorf("want %#v, got %#v", want, got)
     }
 }
