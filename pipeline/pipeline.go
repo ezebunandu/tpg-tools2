16077eb833f40dc342ddf55156b2ffa92c8873b8
Sam Ezebunandu <sam.ezebunandu@gmail.com>
2025-09-01 17:21:13 -0600

    pipeline: create a pipeline from a string or file

---
+++ pipeline/pipeline.go
@@ -0,0 +1,43 @@
+package pipeline
+
+import (
+	"io"
+	"os"
+	"strings"
+)
+
+type Pipeline struct{
+    Reader io.Reader
+    Output io.Writer
+    Error error
+}
+
+func New() *Pipeline {
+    return  &Pipeline{
+        Output: os.Stdout,
+    }
+}
+
+func FromString(s string) *Pipeline{
+    p := New()
+    p.Reader = strings.NewReader(s)
+    return p
+}
+
+func FromFile(pathname string) *Pipeline{
+    p := New()
+    f, err := os.Open(pathname)
+    if err != nil {
+        p.Error = err
+        return  p
+    }
+    p.Reader = f
+    return  p
+}
+
+func (p *Pipeline) Stdout(){
+    if p.Error != nil {
+        return
+    }
+    io.Copy(p.Output, p.Reader)
+}
