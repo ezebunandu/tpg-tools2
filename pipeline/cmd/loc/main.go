f22fe3a318f2c07fafed43c39d62344e9040d53f
Sam Ezebunandu <sam.ezebunandu@gmail.com>
2025-09-04 23:10:50 -0600

    loc: count non-blank lines of go source code in current dir; uses bitfield/script

---
+++ pipeline/cmd/loc/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"regexp"
+
+	"github.com/bitfield/script"
+)
+
+func main(){
+    re := regexp.MustCompile(".go")
+    lines, err := script.FindFiles(".").MatchRegexp(re).Concat().RejectRegexp(regexp.MustCompile(`^\s*$`)).CountLines()
+    if err != nil {
+        fmt.Fprintln(os.Stderr, err)
+        os.Exit(1)
+    }
+    fmt.Printf("You've written %d lines in Go in this project", lines)
+}
