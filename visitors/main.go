7cebcb5f685e85f98339adf9c69a3f430047bcd2
Sam Ezebunandu <sam.ezebunandu@gmail.com>
2025-09-04 23:22:22 -0600

    bugfix: visitor logic, not counting go files -:)

--- visitors/main.go
+++ visitors/main.go
@@ -1,18 +1,10 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"regexp"
 
 	"github.com/bitfield/script"
 )
 
 func main(){
-    re := regexp.MustCompile(".go")
-    n, err := script.FindFiles(".").MatchRegexp(re).Concat().CountLines()
-    if err != nil {
-        log.Fatal(err)
-    }
-    fmt.Println(n)
+    script.File("log.txt").Column(1).Freq().First(10).Stdout()
 }
