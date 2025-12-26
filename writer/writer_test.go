298243ba6a1e30f437afb872c22a7a4640445a6d
Sam Ezebunandu <sam.ezebunandu@gmail.com>
2025-12-26 14:11:40 -0700

    writer: writes given data to path

---
+++ writer/writer_test.go
@@ -0,0 +1,78 @@
+package writer_test
+
+import (
+	"os"
+	"testing"
+
+	"github.com/ezebunandu/writer"
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestWriteToFile__WritesGivenDataToFile(t *testing.T){
+    t.Parallel()
+    path := t.TempDir() + "write_test.txt"
+    want := []byte{1, 2, 3}
+    err := writer.WriteToFile(path, want)
+    if err != nil {
+        t.Fatal(err)
+    }
+    got, err := os.ReadFile(path)
+    if err != nil {
+        t.Fatal(err)
+    }
+    if !cmp.Equal(want, got){
+        t.Error(cmp.Diff(want, got))
+    }
+}
+
+func TestWriteFile__ReturnsErrorForUnwritableFile(t *testing.T){
+    t.Parallel()
+    path := "bogus/write_test.txt"
+    err := writer.WriteToFile(path, []byte{})
+    if err == nil {
+        t.Fatal("want error when file not writable")
+    }
+}
+
+func TestWriteToFile__ClobbersExistingFile(t *testing.T){
+    t.Parallel()
+    path := t.TempDir() + "write_test.txt"
+    err := os.WriteFile(path, []byte{4,5,6}, 0o600)
+    if err != nil {
+        t.Fatal(err)
+    }
+    want := []byte{1, 2, 3}
+    err = writer.WriteToFile(path, want)
+    if err != nil {
+        t.Fatal(err)
+    }
+    got, err := os.ReadFile(path)
+    if err != nil {
+        t.Fatal(err)
+    }
+    if !cmp.Equal(want, got){
+        t.Error(cmp.Diff(want, got))
+    }
+}
+
+func TestWriteToFile__ChangesPermsOnExistingFile(t *testing.T){
+    t.Parallel()
+    path := t.TempDir() + "/perms_test.txt"
+    // Pre-create empty file with open perms
+    err := os.WriteFile(path, []byte{}, 0o666)
+    if err != nil {
+        t.Fatal(err)
+    }
+    err = writer.WriteToFile(path, []byte{1, 2, 3})
+    if err != nil {
+        t.Fatal(err)
+    }
+    stat, err := os.Stat(path)
+    if err != nil {
+        t.Fatal(err)
+    }
+    perm := stat.Mode().Perm()
+    if perm != 0o600{
+        t.Errorf("want file mode 0o600, got 0o%o", perm)
+    }
+}
