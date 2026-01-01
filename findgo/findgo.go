1a13eb9cd592bbba8a4b6f1fd8b261a78b81ca96
Sam Ezebunandu <sam.ezebunandu@gmail.com>
2025-12-31 21:18:15 -0700

    findgo: find go files in tree

---
+++ findgo/findgo.go
@@ -0,0 +1,18 @@
+package findgo
+
+import (
+	"io/fs"
+	"os"
+	"path/filepath"
+)
+
+func Files(path string)(paths []string){
+    fsys := os.DirFS(path)
+    fs.WalkDir(fsys, ".", func(p string, d fs.DirEntry, err error) error {
+        if filepath.Ext(p) == ".go"{
+            paths = append(paths, p)
+        }
+        return nil
+    })
+    return paths
+}
---
+++ findgo/findgo_test.go
@@ -0,0 +1,22 @@
+package findgo_test
+
+import (
+	"testing"
+
+	"github.com/ezebunandu/findgo"
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestFiles__CorrectlyListsFilesInTree(t *testing.T){
+    t.Parallel()
+    want := []string{
+        "file.go",
+        "subfolder/subfolder.go",
+        "subfolder2/another.go",
+        "subfolder2/file.go",
+    }
+    got := findgo.Files("testdata/tree")
+    if !cmp.Equal(want, got){
+        t.Error(cmp.Diff(want, got))
+    }
+}
