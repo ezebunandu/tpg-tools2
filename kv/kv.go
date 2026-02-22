e65efaf9908150b35dea1b4b045154386a0fddf1
Sam Ezebunandu <sam.ezebunandu@gmail.com>
2026-02-22 14:51:50 -0700

    can open a store, write to it, and save

--- kv/kv.go
+++ kv/kv.go
@@ -1,13 +1,35 @@
 package kv
 
+import (
+	"encoding/gob"
+	"errors"
+	"io/fs"
+	"os"
+)
+
 type store struct {
     data map[string]string
+    path string
 }
 
 func OpenStore(path string) (*store, error) {
-	return &store{
+    s := &store{
         data: map[string]string{},
-    }, nil
+        path: path,
+    }
+    f, err := os.Open(path)
+    if errors.Is(err, fs.ErrNotExist) {
+        return s, nil
+    }
+    if err != nil {
+        return nil, err
+    }
+    defer f.Close()
+    err = gob.NewDecoder(f).Decode(&s.data)
+    if err != nil {
+        return nil, err
+    }
+    return s, nil
 }
 
 func (s *store) Get(key string) (string, bool) {
@@ -17,4 +39,13 @@ func (s *store) Get(key string) (string, bool) {
 
 func (s *store) Set(k, v string) {
     s.data[k] = v
+}
+
+func (s *store) Save() error {
+    f, err := os.Create(s.path)
+    if err != nil {
+        return err
+    }
+    defer f.Close()
+    return gob.NewEncoder(f).Encode(s.data)
 }
--- kv/kv_test.go
+++ kv/kv_test.go
@@ -1,8 +1,10 @@
 package kv_test
 
 import (
-	"github.com/ezebunandu/kv"
+	"os"
 	"testing"
+
+	"github.com/ezebunandu/kv"
 )
 
 func TestGet_ReturnsNotOkIfKeyDoesNotExist(t *testing.T) {
@@ -50,4 +52,68 @@ func TestGet_OverwritesExistingValue(t *testing.T) {
         t.Errorf("want 'updated', got %q", v)
     }
     
+}
+
+func TestSave_SavesDataPersistently(t *testing.T) {
+    t.Parallel()
+    path := t.TempDir() + "/kvtest.store"
+    s, err := kv.OpenStore(path)
+    if err != nil {
+        t.Fatal(err)
+    }
+    s.Set("A", "1")
+    s.Set("B", "2")
+    s.Set("C", "3")
+    err = s.Save()
+    if err != nil {
+        t.Fatal(err)
+    }
+    s2, err := kv.OpenStore(path)
+    if err != nil {
+        t.Fatal(err)
+    } 
+    if v, _ := s2.Get("A"); v != "1" {
+        t.Fatalf("want A=1, got A=%s", v)
+    }
+    if v, _ := s2.Get("B"); v != "2" {
+        t.Fatalf("want B=1, got B=%s", v)
+    }
+    if v, _ := s2.Get("C"); v != "3" {
+        t.Fatalf("want C=1, got C=%s", v)
+    }
+}
+
+func TestOpenStore_ErrorsWhenPathUnreadable(t *testing.T){
+    t.Parallel()
+    path := t.TempDir() + "/unreadable.store"
+    if _, err := os.Create(path); err != nil {
+        t.Fatal(err)
+    }
+    if err := os.Chmod(path, 0o000); err != nil {
+        t.Fatal(err)
+    }
+    _, err := kv.OpenStore(path)
+    if err == nil {
+        t.Errorf("want error when path unreadable but got none")
+    }
+}
+
+func TestOpenStore_ReturnsErrorOnInvalidData(t *testing.T){
+    t.Parallel()
+    _, err := kv.OpenStore("testdata/invalid.store")
+    if err == nil {
+        t.Fatal("no error")
+    }
+}
+
+func TestSave_ErrorsWhenPathUnwritable(t *testing.T) {
+    t.Parallel()
+    s, err := kv.OpenStore("bogus/unwritable.store")
+    if err != nil {
+        t.Fatal(err)
+    }
+    err = s.Save()
+    if err == nil {
+        t.Fatal("no error")
+    }
 }
