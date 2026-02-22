3e4b9f22812008b012c54477cc1e993e846334bd
Sam Ezebunandu <sam.ezebunandu@gmail.com>
2026-02-21 22:37:58 -0700

    adds a transcript for a shell session

--- shell/shell.go
+++ shell/shell.go
@@ -13,45 +13,55 @@ type Session struct {
 	Stdin  io.Reader
 	Stdout io.Writer
 	Stderr io.Writer
+    Transcript io.Writer
 	DryRun bool
 }
 
 func NewSession(in io.Reader, out, errs io.Writer) *Session {
-	return &Session{Stdin: in, Stdout: out, Stderr: errs}
+	return &Session{Stdin: in, Stdout: out, Stderr: errs, Transcript: io.Discard}
 }
 
 func CmdFromString(input string) (*exec.Cmd, error) {
 	args := strings.Fields(input)
 	if len(args) < 1 {
-		return &exec.Cmd{}, fmt.Errorf("empty input")
+		return nil, fmt.Errorf("empty input")
 	}
 	return exec.Command(args[0], args[1:]...), nil
 }
 
 func (s *Session) Run() {
-	fmt.Fprint(s.Stdout, "> ")
+    stdout := io.MultiWriter(s.Stdout, s.Transcript)
+    stderr := io.MultiWriter(s.Stderr, s.Transcript)
+	fmt.Fprintf(stdout, "> ")
 	input := bufio.NewScanner(s.Stdin)
 	for input.Scan() {
 		line := input.Text()
 		cmd, err := CmdFromString(line)
 		if err != nil {
-            fmt.Fprintf(s.Stdout, "> ")
+            fmt.Fprintf(stdout, "> ")
 			continue
 		}
         if s.DryRun {
-            fmt.Fprintf(s.Stdout, "%s\n> ", line)
+            fmt.Fprintf(stdout, "%s\n> ", line)
             continue
         }
 		out, err := cmd.CombinedOutput()
 		if err != nil {
-			fmt.Fprintln(s.Stderr, "err", err)
+			fmt.Fprintln(stderr, "err", err)
 		}
-		fmt.Fprintf(s.Stdout, "%s", out)
+		fmt.Fprintf(stdout, "%s> ", out)
 	}
-    fmt.Fprintln(s.Stdout, "\nSee you later!")
+    fmt.Fprintln(stdout, "\nSee you later!")
 }
 
 func Main() {
-    session := NewSession(os.Stdin, os.Stdout, os.Stderr)
+    session := NewSession(os.Stdin, os.Stdout, os.Stderr, )
+    transcript, err := os.Create("transcript.txt")
+    if err != nil {
+        fmt.Fprintln(os.Stderr, err)
+        os.Exit(1)
+    }
+    defer transcript.Close()
+    session.Transcript = transcript
     session.Run()
 }
--- shell/shell_test.go
+++ shell/shell_test.go
@@ -9,7 +9,21 @@ import (
 
 	"github.com/ezebunandu/shell"
 	"github.com/google/go-cmp/cmp"
+	"github.com/rogpeppe/go-internal/testscript"
 )
+
+func TestMain(m *testing.M){
+	testscript.Main(m, map[string]func(){
+		"shell": shell.Main,
+	})
+}
+
+func Test(t *testing.T){
+	t.Parallel()
+	testscript.Run(t, testscript.Params{
+		Dir: "testdata/script",
+	})
+}
 
 func TestCmdFromString_CreatesExpectedCmd(t *testing.T) {
 	input := "/bin/ls -l main.go"
@@ -38,6 +52,7 @@ func TestNewSession_CreatesExpectedSession(t *testing.T) {
 		Stdin:  os.Stdin,
 		Stdout: os.Stdout,
 		Stderr: os.Stderr,
+		Transcript: io.Discard,
 	}
 	got := *shell.NewSession(os.Stdin, os.Stdout, os.Stderr)
 	if want != got {
@@ -57,4 +72,19 @@ func TestRun_ProducesExpectedOutput(t *testing.T){
     if !cmp.Equal(want, got){
         t.Error(cmp.Diff(want, got))
     }
+}
+
+func TestRun_ProducesExpectedTranscript(t *testing.T){
+	t.Parallel()
+	in := strings.NewReader("echo hello\n\n")
+	transcript := new(bytes.Buffer)
+	session := shell.NewSession(in, io.Discard, io.Discard)
+	session.DryRun = true
+	session.Transcript = transcript
+	session.Run()
+	want := "> echo hello\necho hello\n> \n> \nSee you later!\n"
+	got := transcript.String()
+	if !cmp.Equal(want, got){
+		t.Error(cmp.Diff(want, got))
+	}
 }
